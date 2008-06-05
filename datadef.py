0ee802700b2a67c228c6743ceda8faf937151b0c
Kevin Kubasik <kevin@kubasik.net>
2008-06-05 05:09:22 -0700

    stupid aptana

--- datadef.py
+++ datadef.py
@@ -6,14 +6,14 @@ from google.appengine.ext.webapp import template
 
 
 class Photo(db.Model):  
-  author = db.UserProperty()
-  title = db.StringProperty()
-  data = db.BlobProperty()
-  uploaded = db.DateTimeProperty()
+    author = db.UserProperty()
+    title = db.StringProperty()
+    data = db.BlobProperty()
+    uploaded = db.DateTimeProperty()
   
 class Profile(db.Model):
-  user = db.UserProperty()
-  neumont_mail = db.EmailProperty()
-  verify_value = db.StringProperty()
-  valid = db.BooleanProperty()
+    user = db.UserProperty()
+    neumont_mail = db.EmailProperty()
+    verify_value = db.StringProperty()
+    valid = db.BooleanProperty()
   
